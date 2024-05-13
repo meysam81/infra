1937cf87b947da9d1c9b4ea4c1ff35b744c3fa95
Meysam Azad <MeysamAzad81@gmail.com>
2024-05-13 11:38:52 +0700

    feat: add github webhook

---
+++ live/github-webhook/terragrunt.hcl
@@ -0,0 +1,11 @@
+include "root" {
+  path = find_in_parent_folders()
+}
+
+include "aws" {
+  path = find_in_parent_folders("provider_aws.hcl")
+}
+
+include "github" {
+  path = find_in_parent_folders("provider_github.hcl")
+}
---
+++ live/github-webhook/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.2"
+    }
+  }
+  required_version = "< 2"
+}
