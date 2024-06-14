04dd36b5a9e71e49f590747e89600d3502227b04
Meysam Azad <meysam@developer-friendly.blog>
2024-06-14 13:08:45 +0700

    fix(pip): use normal count for tracking public-ip changes

--- kubernetes/raspberry-pi/applications/check-public-ip/app/main.py
+++ kubernetes/raspberry-pi/applications/check-public-ip/app/main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
 import httpx
-import hashlib
 from prometheus_client import start_http_server, Gauge
 
 public_ip_metric = Gauge("public_ip", "Hash of the Public IP address", ["ip_address"])
@@ -12,17 +11,12 @@ def get_public_ip():
     return response.text.strip()
 
 
-def hash_ip(ip_address):
-    return int(hashlib.sha256(ip_address.encode()).hexdigest(), 16)
-
-
 if __name__ == "__main__":
     start_http_server(8000)
 
     while True:
         public_ip = get_public_ip()
-        hashed_ip = hash_ip(public_ip)
 
-        public_ip_metric.labels(ip_address=public_ip).set(hashed_ip)
+        public_ip_metric.labels(ip_address=public_ip).set(1)
 
         time.sleep(60)
