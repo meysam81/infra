6fe17c73edd25dfae49d97f09ef925f90c0f22ea
Meysam Azad <meysam@developer-friendly.blog>
2024-07-03 11:15:14 +0700

    chore(atuin): provide the pg-healthcheck as a probe

---
+++ kubernetes/raspberry-pi/applications/atuin/pg-healthcheck.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -eu
+
+export POSTGRES_HOST=$(echo $ATUIN_DB_URI | awk -F/ '{print $3}' | awk -F@ '{print $2}')
+export POSTGRES_USER=$(echo $ATUIN_DB_URI | awk -F/ '{print $3}' | awk -F: '{print $1}')
+export POSTGRES_PASSWORD=$(echo $ATUIN_DB_URI | awk -F: '{print $3}' | awk -F@ '{print $1}')
+export POSTGRES_DB=$(echo $ATUIN_DB_URI | awk -F/ '{print $4}' | awk -F? '{print $1}')
+
+while true; do
+    if pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -p 5432; then
+        sleep 30
+    else
+        exit 1
+    fi
+done
