e6dc50756d2ef67ee3170600abe1d00a4288bf15
Meysam Azad <meysam@developer-friendly.blog>
2024-09-04 19:59:41 +0700

    feat: deploy listmonk

--- live/newsletter-server/files/prepare-haproxy-certs.sh
+++ live/newsletter-server/files/prepare-haproxy-certs.sh
@@ -3,14 +3,14 @@ set -e
 
 certbot renew -q || echo "Certbot not renewed!"
 
-cd /etc/letsencrypt/live/newsletter.developer-friendly.blog
+cd /etc/letsencrypt/live/developer-friendly.blog
 
 temp_cert=$(mktemp)
 
 cat fullchain.pem privkey.pem > "$temp_cert"
 
-if ! cmp -s "$temp_cert" /etc/haproxy/certs/newsletter.developer-friendly.blog.pem; then
-    mv "$temp_cert" /etc/haproxy/certs/newsletter.developer-friendly.blog.pem
+if ! cmp -s "$temp_cert" /etc/haproxy/certs/developer-friendly.blog; then
+    mv "$temp_cert" /etc/haproxy/certs/developer-friendly.blog
     systemctl reload haproxy
     echo "Certificate updated and HAProxy reloaded."
 else
