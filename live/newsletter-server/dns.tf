62c2b63ef6c710a864b8a1cf64fb86d82fe4b4dd
Meysam Azad <meysam@developer-friendly.blog>
2024-09-17 11:40:50 +0700

    feat: deploy kor with in-house kustomization as their chart sucks

--- live/newsletter-server/dns.tf
+++ live/newsletter-server/dns.tf
@@ -61,3 +61,33 @@ resource "cloudflare_record" "auth" {
   type    = each.key
   content = each.value
 }
+
+resource "cloudflare_record" "vmsingle" {
+  for_each = {
+    A    = hcloud_primary_ip.this["ipv4"].ip_address
+    AAAA = hcloud_primary_ip.this["ipv6"].ip_address
+  }
+
+  zone_id = data.cloudflare_zone.this.id
+
+  name    = "vmsingle"
+  proxied = true
+  ttl     = 1
+  type    = each.key
+  content = each.value
+}
+
+resource "cloudflare_record" "vmagent" {
+  for_each = {
+    A    = hcloud_primary_ip.this["ipv4"].ip_address
+    AAAA = hcloud_primary_ip.this["ipv6"].ip_address
+  }
+
+  zone_id = data.cloudflare_zone.this.id
+
+  name    = "vmagent"
+  proxied = true
+  ttl     = 1
+  type    = each.key
+  content = each.value
+}
