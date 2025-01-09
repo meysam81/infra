93aab297f9e788e3fef6a15bb0970c9081e62cd2
Meysam Azad <meysam@developer-friendly.blog>
2025-01-09 10:55:08 +0700

    chore: scale up the newsletter instance

--- live/newsletter-server/main.tf
+++ live/newsletter-server/main.tf
@@ -2,7 +2,7 @@
 resource "hcloud_server" "this" {
   name        = "personal"
   image       = "ubuntu-24.04"
-  server_type = "cax21"
+  server_type = "cax31"
   location    = "nbg1"
 
   keep_disk = true
